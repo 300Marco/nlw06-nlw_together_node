6c1a552636fdb29415b4f88fefd1cfea5ad54aba
Marco Santana <marcos.maio96@gmail.com>
2021-06-24 20:59:59 -0300

    Made changes to server.js error handling

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,27 @@
 import "reflect-metadata";
-import express from "express"; // Importando express
+import express, { Request, Response, NextFunction } from "express"; // Importando express
+import "express-async-errors";
+import { router } from "../routes";
+import "./database";
+
 const app = express(); // atribuindo o express a uma variável
+
+app.use(express.json());
+
+app.use(router);
+
+app.use( function(err: Error, request: Request, response: Response, next: NextFunction) {
+    if(err instanceof Error) {
+        return response.status(400).json({
+            error: err.message
+        });
+    };
+
+    return response.status(500).json({
+        status: "error",
+        message: "Internal Server Error"
+    });
+});
 
 app.listen("3000", function() {
     console.log("Server is running");
