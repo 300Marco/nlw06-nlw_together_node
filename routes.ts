b6fe865b229709e5e0aa24e690f50fc5e70e5fa9
Marco Santana <marcos.maio96@gmail.com>
2021-06-25 00:11:53 -0300

    Setting up listing of all users available on the platform

--- routes.ts
+++ routes.ts
@@ -8,6 +8,7 @@ import { ensureAuthenticated } from "./src/middlewares/ensureAuthenticated";
 import { ListUserSendComplimentsController } from "./src/controllers/ListUserSendComplimentsController";
 import { ListUserReceiveComplimentsController } from "./src/controllers/ListUserReceiveComplimentsController";
 import { ListTagsController } from "./src/controllers/ListTagsController";
+import { ListUsersController } from "./src/controllers/ListUsersController";
 
 const router = Router();
 
@@ -18,6 +19,7 @@ const createComplimentController = new CreateComplimentController();
 const listUserSendComplimentsController = new ListUserSendComplimentsController();
 const listUserReceiveComplimentsController = new ListUserReceiveComplimentsController();
 const listTagsController = new ListTagsController();
+const listUsersController = new ListUsersController();
 
 router.post("/tags", ensureAuthenticated, ensureAdmin, createTagController.handle);
 router.post("/users", createUserController.handle);
@@ -28,5 +30,6 @@ router.get("/users/compliments/send", ensureAuthenticated, listUserSendComplimen
 router.get("/users/compliments/receive", ensureAuthenticated, listUserReceiveComplimentsController.handle);
 
 router.get("/tags", ensureAuthenticated, listTagsController.handle);
+router.get("/users", ensureAuthenticated, listUsersController.handle);
 
 export { router };
